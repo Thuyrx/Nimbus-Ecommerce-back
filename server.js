3b4a61ed8159d4112bcbc269c6f3cad4d87fb67e
Thuyrx <thynamite408@gmail.com>
2024-10-07 19:05:58 -0400

    configuração v1

--- server.js
+++ server.js
@@ -1,7 +1,47 @@
-import { createServer } from 'node:http'
+import express from 'express'
 
-const server = createServer(() =>{
-    console.log("OII")
+const app = express()
+app.use(express.json())
+
+const users = []
+ 
+//Criar //
+app.post('/usuarios', (req, res) => { 
+
+    users.push(req.body)
+
+    res.status(201).json(req.body)
+})   
+
+//Listar //
+app.get('/usuarios', (req, res) => {
+    res.status(200).json(users)
 })
 
-server.listen(3333)
+app.listen(3000)
+
+
+
+
+// essa parte não configurada //
+app.put('/usuarios', (req, res) => {  //Editar vários//
+
+}) 
+
+
+app.patch('/usuarios', (req, res) => {  //Editar um//
+
+})  
+
+
+app.delete('/usuarios', (req, res) => {  //deletar o usuario //
+
+})  
+
+
+
+
+/* 
+    1- Tipo de Rota / Método HTTP
+    2- Endereço 
+*/
