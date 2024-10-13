2aa1259df35db7bf16a3527841bfaf646a65c82c
Thuyrx <thynamite408@gmail.com>
2024-10-13 16:00:46 -0400

    configurações v6.3

---
+++ src/config/config.js
@@ -0,0 +1,28 @@
+// config/config.js
+import dotenv from 'dotenv';
+dotenv.config(); // Carrega as variáveis de ambiente do .env
+console.log(process.env.DB_PASS)
+
+export default {
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+  },
+  test: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+  },
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+  },
+};
